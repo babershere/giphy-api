3b6692e373807eff2b8a4de69e9b98ddcd74a8cd
Rey Q <rehmatq@gmail.com>
2017-12-02 09:48:50 -0800

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -58,7 +58,7 @@ function addButton() {
 
 function bitchfuck(){
 var x = $(this).attr("data-animal");
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=dc6zaTOxFJmzC&limit=10";
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=dc6zaTOxFJmzC&limit=10";
 console.log(queryURL);
 
 $.ajax({ url: queryURL, method: 'GET' })
